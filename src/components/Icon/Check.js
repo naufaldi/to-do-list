352d78e5830aed0406500368eb817c94557f78ec
naufaldi <naufaldi.rafif@gmail.com>
2021-08-19 04:00:34 +0700

    update readme

--- src/components/Icon/Check.js
+++ src/components/Icon/Check.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Check = ({ className }) => {
   return (
     <svg
-      className="w-6 h-6"
       fill="none"
       stroke="currentColor"
       viewBox="0 0 24 24"
